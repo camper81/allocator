13fcd7631c6a0699c612bbc7541b262829da2f6b
max <camper81@mail.ru>
2022-06-23 14:02:59 +0300

    Added tests

--- src/test.cpp
+++ src/test.cpp
@@ -8,16 +8,31 @@
 #include <exception>
 
 template <typename T>
-void print(T m, std::ostream& os){
+void print_map(T m, std::ostream& os){
     for(auto&[key, value] : m) {
         os << key << " " << value << std::endl;
     }
 }
 
+template <typename T>
+void print_vec(T m, std::ostream& os){
+    for(auto& value : m) {
+        os << value << std::endl;
+    }
+}
+
+
 template <typename T,typename U, size_t Size = 10>
-void assign(T& m, U (*func)(U a)){
+void assign_map(T& m, U (*func)(U a)){
     for(size_t key = 0 ; key < Size; ++key ) {
         m[key] = func(key);
+    }
+}
+
+template <typename T,typename U, size_t Size = 10>
+void assign_vec(T& v, U (*func)(U a)){
+    for(size_t key = 0 ; key < Size; ++key ) {
+        v.push_back(func(key));
     }
 }
 
@@ -29,17 +44,59 @@ void test(T m, std::ostream& os = std::cout){
 
 BOOST_AUTO_TEST_SUITE(test_map)
 
-BOOST_AUTO_TEST_CASE(testCompare) {
+BOOST_AUTO_TEST_CASE(testMapAllocatorByFactorial) {
     std::stringstream sstr1;
     std::map<int, int> m1;
-    assign(m1, &factorial);
-    print(m1, sstr1);
+    assign_map(m1, &factorial);
+    print_map(m1, sstr1);
 
     std::stringstream sstr2;
     std::map<int, int, std::less<int>,
-        CustomAllocator<std::pair<const int, int>, 10>> m2;
-    assign(m2, &factorial);
-    print(m2, sstr2);
+            CustomAllocator<std::pair<const int, int>, 10>> m2;
+    assign_map(m2, &factorial);
+    print_map(m2, sstr2);
+
+    BOOST_CHECK_EQUAL(sstr1.str(), sstr2.str());
+}
+
+BOOST_AUTO_TEST_CASE(testVectorAllocatorByFactorial) {
+    std::stringstream sstr1;
+    std::vector<int> vec1;
+    assign_vec(vec1, &factorial);
+    print_vec(vec1, sstr1);
+
+    std::stringstream sstr2;
+    std::vector<int, CustomAllocator<int, 10>> vec2;
+    assign_vec(vec2, &factorial);
+    print_vec(vec2, sstr2);
+
+    BOOST_CHECK_EQUAL(sstr1.str(), sstr2.str());
+}
+
+BOOST_AUTO_TEST_CASE(testBlankContainer) {
+    std::stringstream sstr1;
+    std::map<int, int> m1;
+    print_map(m1, sstr1);
+
+    std::stringstream sstr2;
+    std::map<int, int, std::less<int>,
+            CustomAllocator<std::pair<const int, int>, 0>> m2;
+    print_map(m2, sstr2);
+
+    BOOST_CHECK_EQUAL(sstr1.str(), sstr2.str());
+}
+
+BOOST_AUTO_TEST_CASE(testBigMapElements) {
+    std::stringstream sstr1;
+    std::map<int, int> m1;
+    assign_map(m1, &factorial);
+    print_map(m1, sstr1);
+
+    std::stringstream sstr2;
+    std::map<int, int, std::less<int>,
+            CustomAllocator<std::pair<const int, int>, 10000>> m2;
+    assign_map(m2, &factorial);
+    print_map(m2, sstr2);
 
     BOOST_CHECK_EQUAL(sstr1.str(), sstr2.str());
 }
@@ -61,18 +118,5 @@ BOOST_AUTO_TEST_SUITE(test_my_container)
 
         BOOST_CHECK_EQUAL_COLLECTIONS(sv1.begin(), sv1.end(), sv2.begin(), sv2.end());
     }
-
-
-    BOOST_AUTO_TEST_CASE(exception_test) {
-//        auto func = [](){
-//            CustomVector<int, CustomAllocator<int, 3>> sv;
-//            for(size_t idx = 0; idx < 10; ++idx) {
-//                sv.push_back(idx);
-//            }
-//        };
-//
-//        BOOST_CHECK_THROW(func(), std::bad_alloc);
-    }
-
 
 BOOST_AUTO_TEST_SUITE_END()
--- src/test_exceptions.cpp
+++ src/test_exceptions.cpp
@@ -7,7 +7,7 @@
 
 BOOST_AUTO_TEST_SUITE(test_exceptions)
 
-BOOST_AUTO_TEST_CASE(exception_test) {
+BOOST_AUTO_TEST_CASE(catchExceptionMyVector) {
         auto func = [](){
             CustomVector<int, CustomAllocator<int, 3>> sv;
             for(size_t idx = 0; idx < 10; ++idx) {
@@ -16,6 +16,31 @@ BOOST_AUTO_TEST_CASE(exception_test) {
         };
 
         BOOST_CHECK_THROW(func(), std::bad_alloc);
+}
+
+BOOST_AUTO_TEST_CASE(catchExceptionVector) {
+    auto func = [](){
+
+        std::vector<int, CustomAllocator<int, 3>> sv;
+        for(size_t idx = 0; idx < 10; ++idx) {
+            sv.push_back(idx);
+        }
+    };
+
+    BOOST_CHECK_THROW(func(), std::bad_alloc);
+}
+
+BOOST_AUTO_TEST_CASE(catchExceptionMap) {
+    auto func = [](){
+
+        std::map<int, int, std::less<int>,
+                CustomAllocator<std::pair<const int, int>, 3>> mp;
+        for(size_t idx = 0; idx < 10; ++idx) {
+            mp[idx] = idx;
+        }
+    };
+
+    BOOST_CHECK_THROW(func(), std::bad_alloc);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
